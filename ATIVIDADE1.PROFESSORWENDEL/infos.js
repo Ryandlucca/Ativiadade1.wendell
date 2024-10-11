44e57b79832315abe9f6047554a6b286358ef602
Ryandlucca <ryandeluccar@gmail.com>
2024-10-10 23:38:52 -0300

    Add files via upload
    ATIVIDADE DE PESQUISA FRONT-END E FRAMEWORK
    PROFESSOR:Wendel

---
+++ ATIVIDADE1.PROFESSORWENDEL/infos.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Cachorro brasileiro",
+        descricao: "Cachorro é um animal é um animal brasileirp",
+        link: "https://love.doghero.com.br/caes/cachorros-fofos/",
+        tags: ["cachorro", "fofo", "animado"]
+    },
+    {
+        titulo: "Gatos",
+        descricao: "Gato é um animal é um animal independente",
+        link: "https://www.petlove.com.br/dicas/gatinhos-fofos-veja-fotos",
+        tags: ["gato", "fofo", "docil"]
+    },
+    {
+        titulo: "capivara",
+        descricao: "capivara é um animal silvestre e proibido de criação",
+        link: "https://www.petlove.com.br/dicas/gatinhos-fofos-veja-fotos",
+        tags: ["diferente", "silvestre", "proibido", ""]
+    }
+];
